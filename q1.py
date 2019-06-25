e2ac18a153ef5c659e38b00350d67974bbc44c5a
KinbiuZhang <43375189+KinbiuZhang@users.noreply.github.com>
2019-06-25 07:57:57 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -19,4 +19,25 @@
 
 def how_many_ways(digitarray):
     # implement here
-    
+    def how_many_ways(digitarray):
+    digitarray = digitarray.lstrip('0')
+    length = len(digitarray)
+    if length == 0:
+        return 0
+    li = list(range(length+1))
+    li[0] = 1
+    for i in range(length+1):
+        if i == 0:
+            continue
+        if digitarray[i-1] == '0':
+            li[i] = 1
+        else:
+            li[i] = li[i-1]
+            #判断当前字符跟前一个字符是否可以凑成一个字母所对应的值
+        if (i>1 and int(digitarray[i-1])<=6 and int(digitarray[i-2])==2) or (i>1 and int(digitarray[i-2])==1):
+                li[i] += li[i-2]
+        # print(li)
+    print(li[length])
+
+if __name__ == '__main__':
+    how_many_ways('119')
